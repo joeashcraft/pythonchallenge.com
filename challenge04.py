bb8295dd0652b784d48e2378afeb39d936082db0
Joe Ashcraft <joeashcraft@gmail.com>
2017-01-11 13:51:12 -0600

    a solution!

--- challenge04.py
+++ challenge04.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python3
 """challenge04.
 
 follow the chain
 """
 
-import urllib
+import requests
 import re
 
 base_url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php'
+param = "nothing"
+token = 12345  # first token
+token = 75635  # near the end
+finished = False
 
-first_page = urllib.urlopen(base_url + '?nothing=12345')
-pagein = first_page.read()
-# print type(pagein)
-print pagein
+while not finished:
+    response = requests.get(base_url, params=dict([(param,token)])).content.decode()
+    print(response)
 
-next_page = re.search('and the next (\w) is (\w)', pagein)
-next_page_param = next_page.group(1)
-next_page_id = next_page.group(2)
+    next_page = re.search('and the next ([a-zA-Z0-9_]*) is ([a-zA-Z0-9_]*)', response)
+    if next_page:
+        param = next_page.group(1)
+        token = next_page.group(2)
+    else:
+        finished = True
